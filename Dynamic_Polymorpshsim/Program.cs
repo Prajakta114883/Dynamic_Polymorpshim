f60a64949888258ae6cf0d9a90224edddf9e87f9
Prajakta Phadatare <prajaktaphadatare112@gmail.com>
2025-04-10 13:44:27 +0530

    Initial push for 30DaysOfLearning

---
+++ Dynamic_Polymorpshsim/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Polymorpshsim
+{
+     abstract class Account
+    {
+        public abstract void AcDetails();
+        public abstract void Withdrwal();
+        public abstract void Deposite();
+        public void Bankdetails()
+        {
+            Console.WriteLine("calling bank details");
+        }
+        class SavingAc : Account
+        {
+            public override void AcDetails()
+            {
+                Console.WriteLine("Calling Account Details of saving account");
+            }
+            public override void Withdrwal()
+            {
+                Console.WriteLine("calling withdrwal of saving account");
+            }
+            public override void Deposite()
+            {
+                Console.WriteLine("calling withdwral of saving account");
+            }
+        }
+        class SallaryAv : Account
+        {
+            public override void AcDetails()
+            {
+                Console.WriteLine("Calling Account Details of saving account");
+            }
+            public override void Withdrwal()
+            {
+                Console.WriteLine("calling withdrwal of saving account");
+            }
+            public override void Deposite()
+            {
+                Console.WriteLine("calling withdwral of saving account");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            SavingAc sv = new SavingAc();
+            sv.Bankdetails();
+            sv.AcDetails();
+            sv.Withdrwal();
+            sv.Bankdetails();
+            sv.Deposite();
+            
+
+            SallaryAv sa = new SallaryAv();
+            sa.Bankdetails();
+            sa.AcDetails();
+            sa.Withdrwal();
+            sa.Bankdetails();
+            sa.Deposite();
+            Console.ReadLine();
+
+        }
+    }
+}
